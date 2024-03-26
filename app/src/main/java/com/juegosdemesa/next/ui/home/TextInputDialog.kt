89c1a173d1e03d0cbe0fc5bf182101a2a6c9adaa
Juan <jamminuesa@gmail.com>
2024-03-26 21:21:05 +0100

    Add Ime action to TextInputDialog

--- app/src/main/java/com/juegosdemesa/next/ui/home/TextInputDialog.kt
+++ app/src/main/java/com/juegosdemesa/next/ui/home/TextInputDialog.kt
@@ -1,33 +1,46 @@
 package com.juegosdemesa.next.ui.home
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogWindowProvider
 
 @Preview
@@ -38,6 +51,7 @@ private fun TextInputDialogPreview(){
         setValue = {})
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun TextInputDialog(
     value: String = "",
@@ -48,6 +62,9 @@ fun TextInputDialog(
 ) {
     val txtFieldError = remember { mutableStateOf("") }
     val txtField = remember { mutableStateOf(value) }
+
+    val focusRequester = remember { FocusRequester() }
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     Dialog(onDismissRequest = { setShowDialog(false) }) {
         (LocalView.current.parent as DialogWindowProvider).window.setDimAmount(0.5f)
@@ -87,7 +104,24 @@ fun TextInputDialog(
 
                     OutlinedTextField(
                         modifier = Modifier
-                            .fillMaxWidth(),
+                            .fillMaxWidth()
+                            .focusRequester(focusRequester)
+                            .onFocusChanged {
+                                if (it.isFocused) {
+                                    keyboardController?.show()
+                                }
+                            },
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                        keyboardActions = KeyboardActions (
+                            onDone = {
+                                if (txtField.value.isEmpty()) {
+                                    txtFieldError.value = "Field can not be empty"
+                                }
+                                setValue(txtField.value)
+                                setShowDialog(false)
+                            }
+                        ),
                         label = { Text(text = hint) },
                         value = txtField.value,
                         onValueChange = {
@@ -118,4 +152,8 @@ fun TextInputDialog(
             }
         }
     }
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 }
+
